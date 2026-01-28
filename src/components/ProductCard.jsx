f0453277ec0ee0b222e8624e487b7dfe78e07e98
dennie <denitsa.sureva@icloud.com>
2026-01-28 19:38:19 +0100

    product screen + product card

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,63 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+export default function ProductCard({ product }) {
+    return (
+        <TouchableOpacity style={styles.card}>
+            <Image
+                source={{ uri: product.imageUrl }}
+                style={styles.image}
+                resizeMode="cover"   
+            />
+
+            <View style={styles.info}>
+                <Text style={styles.name} >
+                    {product.name}
+                </Text>
+
+                <Text style={styles.meta}>
+                    {product.brand} · {product.category}
+                </Text>
+
+
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    card: {
+        flexDirection: "row",
+        backgroundColor: "#FFFFFF",
+        borderRadius: 16,
+        padding: 12,
+        marginBottom: 12,
+
+        // iOS shadow
+        shadowColor: "#000",
+        shadowOpacity: 0.06,
+        shadowRadius: 8,
+        shadowOffset: { width: 0, height: 4 },
+    },
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 12,
+        backgroundColor: "#F4F4F4",
+    },
+    info: {
+        flex: 1,
+        marginLeft: 12,
+        justifyContent: "center",
+    },
+    name: {
+        fontSize: 18,
+        fontWeight: "600",
+        color: "#FFAAB8",
+    },
+    meta: {
+        fontSize: 14,
+        color: "#7A7A7A",
+        marginTop: 2,
+    },
+
+});
--- src/screens/ProductsScreen.jsx
+++ src/screens/ProductsScreen.jsx
@@ -1,65 +1,58 @@
-import { Text, View, StyleSheet, TextInput } from "react-native";
+import { Text, View, StyleSheet, TextInput, ScrollView } from "react-native";
 import { styles } from "../../styles";
+import { products } from "../../data/products";
+import ProductCard from "../components/ProductCard";
 
 export default function ProductsScreen() {
     return (
-
-        <View
-            style={styles.header}
-        >
-            <View style={styles.headerContent}>
-                <View style={currStyles.avatar}></View>
-                <View>
-                    <Text style={styles.title}>Hey you!</Text>
-                    
+        <View style={styles.container}>
+            {/* Header */}
+            <View style={styles.header}>
+                <View style={styles.headerContent}>
+                    <Text style={styles.title}>My Products</Text>
                 </View>
             </View>
-        </View>
 
+            {/* Search */}
+            <View style={currStyles.searchBar}>
+                <TextInput
+                    placeholder="Search"
+                    placeholderTextColor="#9A9A9A"
+                    style={currStyles.search}
+                />
+            </View>
+
+            <View style={currStyles.divider} />;
+            {/* Product List */}
+            <ScrollView contentContainerStyle={currStyles.listContainer}>
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </ScrollView>
+        </View>
     );
-};
+}
 
 const currStyles = StyleSheet.create({
-   
     search: {
-        height: 44,
-        borderRadius: 10,
-        backgroundColor: "#1E1E1E",
+        height: 40,
+        borderRadius: 20,
+        backgroundColor: "#fff",
         paddingHorizontal: 12,
-        color: "#FFFFFF",
+        color: "#FFAAB8",
+        fontSize: 18,
+        width: '95%',
+        borderWidth: 1,
+        borderColor: '#F39EB6',
         marginBottom: 12,
+
     },
-    row: {
-        flexDirection: "row",
-        alignItems: "center",
-        paddingVertical: 14,
-    },
-    icon: {
-        width: 48,
-        height: 48,
-        borderRadius: 10,
-        marginRight: 12,
-    },
-    textContainer: {
-        flex: 1,
-    },
-    title: {
-        color: "#FFFFFF",
-        fontSize: 16,
-        fontWeight: "600",
-    },
-    subtitle: {
-        color: "#9A9A9A",
-        fontSize: 13,
-        marginTop: 2,
-    },
-    chevron: {
-        color: "#6E6E6E",
-        fontSize: 24,
-        marginLeft: 8,
-    },
-    separator: {
-        height: StyleSheet.hairlineWidth,
-        backgroundColor: "#2A2A2A",
+    searchBar: {
+        alignItems: 'center',
+    }, 
+    divider: {
+        height: 1,
+        backgroundColor: "#EDEDED",
+        marginVertical: 10,
     },
 });
