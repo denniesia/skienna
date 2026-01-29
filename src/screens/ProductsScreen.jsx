1f774afe2cf825b71caa5b50f9386065e559f9e6
dennie <denitsa.sureva@icloud.com>
2026-01-29 19:16:17 +0100

    add product and archive icon on productsscreen

--- src/screens/ProductsScreen.jsx
+++ src/screens/ProductsScreen.jsx
@@ -1,8 +1,11 @@
-import { Text, View, StyleSheet, TextInput, ScrollView } from "react-native";
+import { Text, View, StyleSheet, TextInput, ScrollView, Pressable, TouchableOpacity } from "react-native";
 import { styles } from "../../styles";
 import { products } from "../../data/products";
 import ProductCard from "../components/ProductCard";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from '@expo/vector-icons'; 
+import { AntDesign } from '@expo/vector-icons';
+
 
 export default function ProductsScreen() {
     return (
@@ -10,8 +13,20 @@ export default function ProductsScreen() {
             <SafeAreaView style={[styles.container]}>
                 {/* Header */}
                 <View style={styles.header}>
-                    <View style={styles.headerContent}>
+                   <View style={[styles.headerContent, {justifyContent: 'space-between'}]}>
+                        {/* Title */}
                         <Text style={styles.title}>My Products</Text>
+
+                        {/* Right Icons */}
+                        <View style={currStyles.iconContainer}>
+                            <TouchableOpacity onPress={() => console.log("Archive pressed")} style={styles.iconButton}>
+                                <Ionicons name="archive" size={28} color="#F39EB6" />
+                            </TouchableOpacity>
+
+                            <TouchableOpacity onPress={() => console.log("Add pressed")} style={styles.iconButton}>
+                                <AntDesign name="plus" size={28} color="#F39EB6" />
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
 
@@ -57,5 +72,9 @@ const currStyles = StyleSheet.create({
         height: 1,
         backgroundColor: "#EDEDED",
         marginVertical: 10,
+    },
+    iconContainer: {
+        flexDirection: "row",
+        gap: 28, 
     },
 });
