d47720ad63c4f8a00b4f735680192a782db78619
dennie <denitsa.sureva@icloud.com>
2026-02-21 19:51:05 +0100

    login screen

---
+++ src/screens/auth/LoginScreen.jsx
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    Alert,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    Image
+
+} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        if (!email || !password) {
+            Alert.alert('Error', 'Please enter email and password');
+            return;
+        }
+
+        // Replace with real authentication logic
+        Alert.alert('Success', `Email: ${email}`);
+    };
+
+    return (
+        <SafeAreaProvider>
+            r<SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <ScrollView contentContainerStyle={styles.scroll}>
+          
+          {/* Top Image */}
+          {/* <Image
+            source={require("../../../assets/moon.png")} 
+            style={styles.image}
+            resizeMode="contain"
+          /> */}
+
+          {/* Card */}
+          <View style={styles.card}>
+            <Text style={styles.title}>Welcome Back </Text>
+
+            <View style={styles.inputContainer}>
+              <Text style={styles.label}>Email</Text>
+              <TextInput
+                placeholder="you@email.com"
+                placeholderTextColor="#aaa"
+                style={styles.input}
+                value={email}
+                onChangeText={setEmail}
+              />
+            </View>
+
+            <View style={styles.inputContainer}>
+              <Text style={styles.label}>Password</Text>
+              <TextInput
+                placeholder="••••••••"
+                placeholderTextColor="#aaa"
+                secureTextEntry
+                style={styles.input}
+                value={password}
+                onChangeText={setPassword}
+              />
+            </View>
+
+            <TouchableOpacity style={styles.loginBtn} onPress={() => console.log('login')}>
+              <Text style={styles.loginText}>Login</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Register")}
+            >
+              <Text style={styles.registerText}>
+                Don't have an account? Sign up
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+        </SafeAreaProvider>
+    );
+};
+
+export default LoginScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  scroll: {
+    flexGrow: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 220,
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    padding: 20,
+   
+
+    shadowColor: "#000",
+    shadowOpacity: 0.08,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 5 },
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "700",
+    color: "#f376b4",
+    textAlign: "center",
+    marginBottom: 25,
+  },
+  inputContainer: {
+    marginBottom: 15,
+  },
+  label: {
+    color: "#FF69B4",
+    fontSize: 16,
+    marginBottom: 6,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#F2BED1",
+    borderRadius: 10,
+    padding: 12,
+    fontSize: 16,
+  },
+  loginBtn: {
+    backgroundColor: "#F39EB6",
+    padding: 14,
+    borderRadius: 12,
+    marginTop: 10,
+  },
+  loginText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  registerText: {
+    textAlign: "center",
+    marginTop: 18,
+    color: "#f376b4",
+    fontWeight: "600",
+  },
+});
