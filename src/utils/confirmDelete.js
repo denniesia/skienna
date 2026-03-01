8e0de28cc6637d04df30a8f82c89b416018d7a71
dennie <denitsa.sureva@icloud.com>
2026-03-01 14:55:47 +0100

    delete func in utils

---
+++ src/utils/confirmDelete.js
@@ -0,0 +1,12 @@
+import { Alert } from "react-native";
+
+export const confirmDelete = ({ title, message, onConfirm }) => {
+  Alert.alert(
+    title,
+    message,
+    [
+      { text: "Cancel", style: "cancel" },
+      { text: "Delete", style: "destructive", onPress: onConfirm }
+    ]
+  );
+};
